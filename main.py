1530d1d3e17f9b9a655e848f62bc0ac5c8cef08a
tr4k87 <110128836+tr4k87@users.noreply.github.com>
2022-10-26 01:22:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from model_2 import *
 
 
-app = ApplicationBuilder().token("5783069909:AAHJNWvWSA8mnZw-QvK4SCxZQdqWI1NwSD0").build()
+app = ApplicationBuilder().token("TOKEN").build()
 
 app.add_handler(CommandHandler("enter", enter))
 app.add_handler(CommandHandler("w_txt", w_txt))
